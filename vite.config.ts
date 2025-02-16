13f3d28e50dd4780c2766dcdbb06655e791b00d9
vulgrimmm <wadahjedda3@gmail.com>
2025-02-16 13:38:05 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/vulgrimmm.github.io/', // Add this line - should match your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
